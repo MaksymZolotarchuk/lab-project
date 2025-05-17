b256a251e905af7c0da5d8cdd0ea583a31622c8f
MaksymZolotarchuk <146109073+MaksymZolotarchuk@users.noreply.github.com>
2025-05-17 20:32:18 +0300

    Delete jest.config.js

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  collectCoverage: true,
-  coverageReporters: ['html', 'text-summary'],
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
-  moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-  },
-};
