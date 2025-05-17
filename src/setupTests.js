b335017d52208224126005a4a8eb4805ff94ccd3
MaksymZolotarchuk <146109073+MaksymZolotarchuk@users.noreply.github.com>
2025-05-17 20:31:39 +0300

    Delete src directory

--- src/setupTests.js
+++
@@ -1,8 +0,0 @@
-import '@testing-library/jest-dom';
-
-// Mock for react-router-dom
-jest.mock('react-router-dom', () => ({
-    ...jest.requireActual('react-router-dom'),
-    useNavigate: () => jest.fn(),
-    useParams: () => ({ id: '1' }),
-}));
